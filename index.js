079ecd84ed1144cb8bd7393d3a320c26be3fc374
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 21:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ function setThisWithCall(fn, thisValue, arguments){
   return fn.call(thisValue, arguments);
 }
 
-
-
-function setThisWithApply(fn, thisValue, [arguments]){
-  return fn.call(thisValue, arguments);
-}
